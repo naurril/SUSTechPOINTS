8f4b038908e1bd68dcd6ec6aa215e8e669a8499a
Naurril <naurril@gmail.com>
2020-04-01 09:33:17 +0800

    linear interpolation

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ env = Environment(loader=FileSystemLoader('./'))
 
 import os
 import sys
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# sys.path.append(BASE_DIR)
-# sys.path.append(os.path.join(BASE_DIR, '../pointnet'))
-# import predict
+import scene_reader
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../DeepAnnotate'))
+import predict
+
+sys.path.append(os.path.join(BASE_DIR, '../tracking'))
+import trajectory_predict
 
 extract_object_exe = "~/code/pcltest/build/extract_object"
 registration_exe = "~/code/go_icp_pcl/build/test_go_icp"
@@ -50,31 +55,29 @@ class Root(object):
         f.write(rawbody)
       
       return "ok"
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def interpolate(self, scene, frame, obj_id):
+      interpolate_num = trajectory_predict.predict(scene, obj_id, frame, None)
+      return interpolate_num
 
 
     # data  N*3 numpy array
     @cherrypy.expose    
     @cherrypy.tools.json_out()
     def predict_rotation(self):
-      # cl = cherrypy.request.headers['Content-Length']
-      # rawbody = cherrypy.request.body.read(int(cl))
+      cl = cherrypy.request.headers['Content-Length']
+      rawbody = cherrypy.request.body.read(int(cl))
       
-      # data = json.loads(rawbody)
+      data = json.loads(rawbody)
       
-      # return {"angle": predict.predict(data["points"])}
-      return {}
+      return {"angle": predict.predict(data["points"])}
+      #return {}
 
     @cherrypy.expose    
     @cherrypy.tools.json_out()
     def load_annotation(self, scene, frame):
-      filename = "./data/"+scene +"/label/"+ frame + ".json"
-      if (os.path.isfile(filename)):
-        with open(filename,"r") as f:
-          ann=json.load(f)
-          print(ann)          
-          return ann
-      else:
-        return []
+      return scene_reader.read_annotations(scene, frame)
 
     @cherrypy.expose    
     @cherrypy.tools.json_out()
@@ -112,152 +115,7 @@ class Root(object):
     @cherrypy.expose    
     @cherrypy.tools.json_out()
     def datameta(self):
-      data = []
-
-      scenes = os.listdir("./data")
-      print(scenes)
-      scenes.sort()
-
-      for s in scenes:
-        scene = {
-          "scene": s,
-          "frames": []
-        }
-
-        if os.path.exists(os.path.join("./data", s, "disable")):
-          print(s, "disabled")
-          continue
-        
-        data.append(scene)
-
-        frames = os.listdir("./data/"+s+"/pcd")
-        #print(s, frames)
-        frames.sort()
-
-        scene["pcd_ext"]="pcd"
-        for f in frames:
-          #if os.path.isfile("./data/"+s+"/pcd/"+f):
-            filename, fileext = os.path.splitext(f)
-            scene["frames"].append(filename)
-            scene["pcd_ext"] = fileext
-        
-        point_transform_matrix=[]
-
-        if os.path.isfile("./data/"+s+"/point_transform.txt"):
-          with open("./data/"+s+"/point_transform.txt")  as f:
-            point_transform_matrix=f.read()
-            point_transform_matrix = point_transform_matrix.split(",")
-
-        def strip_str(x):
-          return x.strip()
-
-
-        
-        # calib_file = "./data/"+s+"/calib.txt"
-        # if os.path.isfile(calib_file):
-        #   calib["image"] = {}
-        #   with open(calib_file)  as f:
-        #     lines = f.readlines()
-        #     calib["image"]["extrinsic"] = map(strip_str, lines[0].strip().split(","))
-        #     calib["image"]["intrinsic"] = lines[1].strip().split(",") 
-
-        # calibleft={}
-        # calib_file = "./data/"+s+"/calib_left.txt"
-        # if os.path.isfile(calib_file):
-        #   calib["left"] = {}
-        #   with open(calib_file)  as f:
-        #     lines = f.readlines()
-        #     calib["left"]["extrinsic"] = map(strip_str, lines[0].strip().split(","))
-        #     calib["left"]["intrinsic"] = lines[1].strip().split(",") 
-
-        # calibright={}
-        # calib_file = "./data/"+s+"/calib_right.txt"
-        # if os.path.isfile(calib_file):
-        #   calib["right"] = {}
-        #   with open(calib_file)  as f:
-        #     lines = f.readlines()
-        #     calib["right"]["extrinsic"] = map(strip_str, lines[0].strip().split(","))
-        #     calib["right"]["intrinsic"] = lines[1].strip().split(",") 
-        calib={}
-        if os.path.exists("./data/"+s+"/calib"):
-          calibs = os.listdir("./data/"+s+"/calib")
-          for c in calibs:
-            calib_file = "./data/"+s+"/calib/" + c
-            calib_name, _ = os.path.splitext(c)
-            if os.path.isfile(calib_file):
-              print(calib_file)
-              with open(calib_file)  as f:
-                cal = json.load(f)
-                calib[calib_name] = cal
-
-        # camera names
-        image = []
-        image_ext = ""
-        cam_path = "./data/"+s+"/image"
-        if os.path.exists(cam_path):
-          if os.path.exists(cam_path):
-            cams = os.listdir(cam_path)
-            for c in cams:
-              cam_file = "./data/"+s+"/image/" + c
-              if os.path.isdir(cam_file):
-
-                if image:
-                  image.append(c)
-                else:
-                  image = [c]
-
-                if image_ext == "":
-                  #detect image file ext
-                  files = os.listdir(cam_file)
-                  if len(files)>=2:
-                    _,image_ext = os.path.splitext(files[0])
-
-        if image_ext == "":
-          image_ext = ".jpg"
-        scene["image_ext"] = image_ext
-
-
-        if not os.path.isdir("./data/"+s+"/bbox.xyz"):
-          scene["boxtype"] = "psr"
-          if point_transform_matrix:
-            scene["point_transform_matrix"] = point_transform_matrix
-          if calib:
-            scene["calib"] = calib
-          if image:
-            scene["image"] = image
-        else:
-          scene["boxtype"] = "xyz"
-          if point_transform_matrix:
-            scene["point_transform_matrix"] = point_transform_matrix
-          if calib:
-            scene["calib"] = calib
-          if image:
-            scene["image"] = image
-
-
-      print(data)
-      return data
-      # return [
-      #         {
-      #           "scene":"liuxian1",
-      #           "frames": [
-      #             "000242","000441"
-      #           ],
-      #           "boxtype":"xyz",
-      #           "point_transform_matrix": [
-      #             1, 0, 0, 
-      #             0, 0, 1, 
-      #             0, -1, 0,
-      #           ]
-      #         },
-      #         {
-      #           "scene":"liuxian2",
-      #           "frames": [
-      #             "test"
-      #           ],
-      #           "boxtype":"psr",
-      #         },
-      #        ]
+      return scene_reader.get_all_scenes()
     
     @cherrypy.expose    
     @cherrypy.tools.json_out()
