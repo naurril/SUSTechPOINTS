2dcdbbee8e6c33996b9d8069917a7714d420c94d
Naurril <naurril@gmail.com>
2020-04-29 13:36:52 +0800

    log window: added clear button

--- public/js/log.js
+++ public/js/log.js
@@ -2,15 +2,24 @@ function Log(){
     this.div = document.querySelector("#output-window #textarea");
 
     this.handle = document.querySelector("#output-window #handle");
+    this.clear = document.querySelector("#output-window #clear");
 
     this.handle.onclick = (event)=>{
         if (this.div.style.display===""){
             this.div.style.display="none";
+            this.clear.style.display="none";
         }else{
             this.div.style.display="";
+            this.clear.style.display="";
         }
     };
 
+    this.clear.onclick = (evnet) =>{
+        event.stopPropagation();
+        event.preventDefault();
+
+        this.div.innerHTML = "";
+    }
 
     this.println = function(str){
         console.log(str);
