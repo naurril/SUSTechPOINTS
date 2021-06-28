c3b5b3e59eec2cc3f1422209c7824ffd56b3345a
naurril <naurril@gmail.com>
2021-06-28 22:08:41 +0800

    generate new obj id

--- public/js/obj_id_list.js
+++ public/js/obj_id_list.js
@@ -21,7 +21,7 @@ function load_obj_ids_of_scene(scene, done){
                 return "<option value="+c.id+">"+c.category+"</option>";
             }).reduce(function(x,y){return x+y;}, 
                             //"<option value='auto'></option><option value='new'></option>");
-                            "<option value='new'></option>");
+                            "<option value='new'>suggest a new id</option>");
 
             obj_id_list = ret.map(function(x){return x.id;});
 
