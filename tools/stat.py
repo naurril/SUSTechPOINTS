7214318248ebf5ed431be33a27663f37a508018d
naurril <naurril@gmail.com>
2022-12-23 16:23:35 +0800

    stat tool

--- tools/stat.py
+++ tools/stat.py
@@ -5,7 +5,8 @@ import os
 import json
 
 
-stat = {}
+obj_type_stat = {}
+track_stat = {}
 def stat_scene(scene):
     
     label_folder = os.path.join("./", scene, "label")
@@ -16,7 +17,7 @@ def stat_scene(scene):
             continue
 
         objs = []
-
+        
         if not os.path.exists(os.path.join(label_folder, file)):
             continue
 
@@ -29,20 +30,29 @@ def stat_scene(scene):
                 objs = labels
 
             
-            for l in objs:
-                #color = get_color(l["obj_id"])
-                obj_type = l["obj_type"]
-                if stat.get(obj_type):
-                    stat[obj_type] =  stat[obj_type] + 1
-                else:
-                    stat[obj_type] = 1
+        for l in objs:
+            #color = get_color(l["obj_id"])
+            obj_type = l["obj_type"]
+            if obj_type in obj_type_stat:
+                obj_type_stat[obj_type] += 1
+            else:
+                obj_type_stat[obj_type] = 1
+            
+            obj_track = scene+"-"+l['obj_id']
+            if obj_track in track_stat:
+                track_stat[obj_track] += 1
+            else:
+                track_stat[obj_track] = 1
 
-    return stat
+    return obj_type_stat
 
 if __name__=="__main__":
     for s in os.listdir("./"):
         if os.path.isdir(s):
-            print("stat {}".format(s))
-            stat = stat_scene(s)
-    for x in stat:
-        print(x, stat[x])
+            print("scan {}".format(s))
+            obj_type_stat = stat_scene(s)
+    for x in obj_type_stat:
+        print(x, obj_type_stat[x])
+
+    for x in track_stat:
+        print(x, track_stat[x])
