26cd400d88e8cd2cc1b93812992c1d9ec223cf9c
Gurveer Sohal <gurveersohal433@gmail.com>
2023-08-22 13:11:37 -0600

    format

--- public/js/main.js
+++ public/js/main.js
@@ -1,64 +1,57 @@
-import{Config} from "./config.js"
-import{Editor} from "./editor.js"
-import {Data} from './data.js'
+import { Config } from "./config.js";
+import { Editor } from "./editor.js";
+import { Data } from "./data.js";
 
-
-let pointsGlobalConfig = new Config();
+let pointsGlobalConfig = new Config(); // an object
 window.pointsGlobalConfig = pointsGlobalConfig;
 
+pointsGlobalConfig.load(); // will populate the object with key value pairs
 
-pointsGlobalConfig.load();
+document.documentElement.className = "theme-" + pointsGlobalConfig.theme; // for switching between light and dark mode?
 
-
-document.documentElement.className="theme-"+pointsGlobalConfig.theme;
-
-
-document.body.addEventListener('keydown', event => {
-    if (event.ctrlKey && 'asdv'.indexOf(event.key) !== -1) {
-      event.preventDefault()
-    }
+document.body.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && "asdv".indexOf(event.key) !== -1) {
+    event.preventDefault();
+  }
 });
 
-async function createMainEditor(){
-
-  let template = document.querySelector('#editor-template');
-  let maindiv  = document.querySelector("#main-editor");
+async function createMainEditor() {
+  let template = document.querySelector("#editor-template");
+  let maindiv = document.querySelector("#main-editor");
   let main_ui = template.content.cloneNode(true);
   maindiv.appendChild(main_ui); // input parameter is changed after `append`
 
   let editorCfg = pointsGlobalConfig;
 
   let dataCfg = pointsGlobalConfig;
-  
+
   let data = new Data(dataCfg);
   await data.init();
 
-  let editor = new Editor(maindiv.lastElementChild, maindiv, editorCfg, data, "main-editor")
+  let editor = new Editor(
+    maindiv.lastElementChild,
+    maindiv,
+    editorCfg,
+    data,
+    "main-editor"
+  );
   window.editor = editor;
   editor.run();
   return editor;
-} 
+}
 
-async function start(){
-
- 
+async function start() {
   let mainEditor = await createMainEditor();
 
-
-  let url_string = window.location.href
+  let url_string = window.location.href;
   let url = new URL(url_string);
   //language
   let scene = url.searchParams.get("scene");
   let frame = url.searchParams.get("frame");
 
-  if (scene && frame)
-  {
+  if (scene && frame) {
     mainEditor.load_world(scene, frame);
   }
 }
 
-
-
-
 start();
-
