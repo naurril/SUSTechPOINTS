3f86d026d8a11343b9450c06f15a58064ab2b6ee
echeng-git <>
2023-08-24 13:13:15 -0600

    Outputs in global and also added output folder

--- prepare_pcd.py
+++ prepare_pcd.py
@@ -1,6 +1,4 @@
 import laspy
-import file_tools
-import gen_traj
 import argparse
 import open3d as o3d
 import numpy as np
@@ -8,8 +6,12 @@ import numpy as np
 import os, sys
 from pathlib import Path
 
+import tkinter as tk
+from tkinter import Tk
+from tkinter import filedialog
+
 '''
-Converts a .las file to .pcd and then shifts it to local.
+Converts a .las file to .pcd for the annotation tool, though temporary.
 '''
 
 # Global variables for file I/O
@@ -21,12 +23,11 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))
 
 # Centers point cloud and applies colormap
-def convert_pcd(las: laspy.LasData, road_pts: np.ndarray, filename: str) -> None:
-    print("\nConverting point cloud.")
+def convert_pcd(las: laspy.LasData, output_folder: str, filename: str) -> None:
+    print("\nConverting point cloud...")
     pcd = o3d.geometry.PointCloud()
     
     las_xyz = las.xyz
-    las_xyz -= road_pts[road_pts.shape[0] // 2, :] # Center point cloud
     
     pcd.points = o3d.utility.Vector3dVector(las_xyz)
     print("Points loaded.")
@@ -36,14 +37,11 @@ def convert_pcd(las: laspy.LasData, road_pts: np.ndarray, filename: str) -> None
     las_intensity = las.intensity
     
     # Normalize intensity values to [0, 1], then assign RGB values
+    # The intensity of our .las files is usually from 0 to 65535 
     normalizer = matplotlib.colors.Normalize(np.min(las_intensity), np.max(las_intensity))
     las_rgb = matplotlib.cm.gray(normalizer(las_intensity))[:,:-1]
     pcd.colors = o3d.utility.Vector3dVector(las_rgb) # cmap(las_intensity) returns RGBA, cut alpha channel
     print("Intensity colors loaded.")
-    
-    output_folder = os.path.join(ROOT2, "converted")
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
 
     print("Saving to file...")
     output_path = os.path.join(output_folder, f"{filename}_converted.pcd")    
@@ -54,9 +52,6 @@ def convert_pcd(las: laspy.LasData, road_pts: np.ndarray, filename: str) -> None
 
 # Obtains raw las data that we will convert on
 def open_las(args: argparse.Namespace):
-    
-    import tkinter as tk
-    from tkinter import Tk
     
     try:
         arg_input = args.input
@@ -85,36 +80,57 @@ def open_las(args: argparse.Namespace):
 
     return raw_las, las_filename_cut
 
-# We need to get the road point that represents the center of the original point cloud
-def get_trajectory(las: laspy.LasData, args: argparse.Namespace) -> np.ndarray:
-    traj_config = gen_traj.TrajectoryConfig(floor_box_edge=2.0, point_density=1.0, observer_height=1.8)
-    las_obj = file_tools.LasPointCloud(
-        las.x,
-        las.y,
-        las.z,
-        las.gps_time,
-        las.scan_angle_rank,
-        las.point_source_id,
-        las.intensity,
-        None,
-    )
+# Get the output folder
+def obtain_output_folder(args: argparse.Namespace) -> str:
     
-    road_points, _, _, _ = gen_traj.generate_trajectory(
-        verbose=False, 
-        las_obj=las_obj,
-        traj=traj_config
+    try:
+        arg_output = args.output
+    except AttributeError:
+        arg_output = None
+    
+    if arg_output == None:
+        # Obtain the folder for output manually
+        Tk().withdraw()
+        output_folder = tk.filedialog.askdirectory(
+            initialdir=os.path.join(ROOT2, "data"), title="Please select the output LiDAR folder (where the .pcd files are)"
         )
+        output_folder = output_folder + '/'
+    else:
+        output_folder = f"{ROOT2.as_posix()}/{args.output}"
     
-    print("Trajectory generation complete.")
+    # Check for any errors in the path
+    # Output folder must be two subfolders from the root directory 
+    # and the last folder should be named 'lidar'
+    # I could probably autogenerate a proper folder but there's not too much of a point
+    file_depth = len(output_folder.split('/')[:-1]) - len(ROOT2.as_posix().split('/'))
     
-    return road_points
+    if not (file_depth == 3 and output_folder.split('/')[-2] == 'lidar'):
+        raise RuntimeError(f"Incorrect output folder! Make sure that the output folder selected is data/<YOUR_OUTPUT_FOLDER>/lidar!")
+
+    print(f"The converted point cloud will be written to {output_folder}")
+
+    return output_folder
+
+# Get command line arguments
+def parse_cmdline_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "--input", type=str, default=None, help="Path to the .las file"
+    )
+
+    parser.add_argument(
+        "--output", type=str, default=None, help="Path to the output folder"
+    )
+
+    return parser.parse_args()
 
 def main():
-    args = file_tools.parse_cmdline_args()
+    args = parse_cmdline_args()
     las, las_filename_cut = open_las(args)
-    road_points = get_trajectory(las, args)
+    output_folder = obtain_output_folder(args)
     
-    convert_pcd(las, road_points, las_filename_cut)
+    convert_pcd(las, output_folder, las_filename_cut)
   
     return
 
